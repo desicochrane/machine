aec60a061d555e1c8d42fc5b10e43b1444e3528a
Desi Cochrane <desi@desicochrane.com>
2018-08-21 11:15:16 +0200

    Initial Commit

---
+++ src/StateMachine.js
@@ -0,0 +1,68 @@
+export default function(startState, onError) {
+    const stateFns = {}
+
+    function start(model, config = {}) {
+        const logging = config.logging === true
+
+        const machine = {
+            model,
+            state: null,
+            setState,
+            dispatch,
+        }
+
+        function err(msg) {
+            if (onError) {
+                onError(machine, msg)
+            } else {
+                throw Error(msg)
+            }
+        }
+
+        function dispatch(event, data) {
+            if (machine.state === null) {
+                err(`no start state set`)
+            }
+
+            if (logging) console.log(`dispatch: ${machine.state}:${event}`, data)
+
+            const events = stateFns[machine.state]
+            if (!events) {
+                err(`transition ${machine.state}:${event} not defined`)
+            }
+
+            const fn = stateFns[machine.state][event]
+            if (!fn) {
+                err(`transition ${machine.state}:${event} not defined`)
+            }
+
+            fn(machine, data)
+        }
+
+        function setState(state) {
+            if (logging) console.log(`transition: ${machine.state} -> ${state}`)
+
+            machine.state = state
+        }
+
+        setState(startState)
+
+        return machine
+    }
+
+    function transition(state, event, fn) {
+        if (!stateFns[state]) stateFns[state] = {}
+
+        if (stateFns[state][event]) {
+            throw new Error(`transition ${state}:${event} already defined`)
+        }
+
+        stateFns[state][event] = fn
+    }
+
+    return {
+        start,
+        transition,
+    }
+}
+
---
+++ tests/StateMachineTest.js
@@ -0,0 +1,88 @@
+import { expect } from 'chai'
+import StateMachine from '../src/StateMachine'
+
+describe('StateMachine', () => {
+
+    it('can create a new "StateMachine"', (done) => {
+        const Model = {
+            username: '',
+            err: null,
+        }
+
+        const EvtEdit = 'Edit'
+        const EvtSubmit = 'Submit'
+        const EvtResponseOK = 'ResponseOK'
+        const EvtResponseErr = 'ResponseErr'
+
+        const EditState = 'EditState'
+        const SubmitState = 'SubmitState'
+        const SuccessState = 'SuccessState'
+
+        const builder = StateMachine(EditState)
+
+        builder.transition(EditState, EvtEdit, (sm, { username }) => {
+            sm.model.username = username
+        })
+
+        builder.transition(EditState, EvtSubmit, (sm, { fail }) => {
+            sm.model.err = null
+
+            sm.setState(SubmitState)
+
+            setTimeout(() => {
+                if (fail === true) {
+                    sm.dispatch(EvtResponseErr, { err: 'Invalid' })
+                } else {
+                    sm.dispatch(EvtResponseOK)
+                }
+            }, 1)
+        })
+
+        builder.transition(SubmitState, EvtResponseOK, (sm) => {
+            sm.setState(SuccessState)
+        })
+
+        builder.transition(SubmitState, EvtResponseErr, (sm, { err }) => {
+            sm.model.err = err
+            sm.setState(EditState)
+        })
+
+        const machine = builder.start(Model)
+
+        expect(machine.state).to.equal(EditState)
+        expect(machine.model.username).to.equal('')
+        expect(machine.model.err).to.equal(null)
+
+        machine.dispatch(EvtEdit, { username: 'Desio' })
+
+        expect(machine.state).to.equal(EditState)
+        expect(machine.model.username).to.equal('Desio')
+        expect(machine.model.err).to.equal(null)
+
+        machine.dispatch(EvtSubmit, { fail: true })
+
+        expect(machine.state).to.equal(SubmitState)
+        expect(machine.model.username).to.equal('Desio')
+        expect(machine.model.err).to.equal(null)
+
+        setTimeout(() => {
+            expect(machine.state).to.equal(EditState)
+            expect(machine.model.username).to.equal('Desio')
+            expect(machine.model.err).to.equal('Invalid')
+
+            machine.dispatch(EvtSubmit, { fail: false })
+
+            expect(machine.state).to.equal(SubmitState)
+            expect(machine.model.username).to.equal('Desio')
+            expect(machine.model.err).to.equal(null)
+
+            setTimeout(() => {
+                expect(machine.state).to.equal(SuccessState)
+                expect(machine.model.username).to.equal('Desio')
+                expect(machine.model.err).to.equal(null)
+
+                done()
+            }, 1)
+        }, 1)
+    })
+})
