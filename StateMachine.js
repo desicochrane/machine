9e1bff9e70df43807b97cf2c8229391c8ae3a901
Desi Cochrane <desi@desicochrane.com>
2018-08-22 14:39:19 +0200

    cleanup

--- StateMachine.js
+++ StateMachine.js
@@ -4,20 +4,11 @@ export default function(startState, onError) {
     function start(model, config = {}) {
         const logging = config.logging === true
 
-        const machine = {
-            model,
-            state: null,
-            setState,
-            inState,
-            dispatch,
-        }
+        const machine = { state: null, model, setState, inState, dispatch }
 
         function err(msg) {
-            if (onError) {
-                onError(machine, msg)
-            } else {
-                throw Error(msg)
-            }
+            if (onError) return onError(machine, msg)
+            throw Error(msg)
         }
 
         function inState() {
@@ -25,48 +16,34 @@ export default function(startState, onError) {
         }
 
         function dispatch(event, data) {
-            if (machine.state === null) {
-                err(`no start state set`)
-            }
+            if (machine.state === null) err(`no start state set`)
 
             if (logging) console.log(`dispatch: ${machine.state}:${event}`, data)
 
             const events = stateFns[machine.state]
-            if (!events) {
-                err(`transition ${machine.state}:${event} not defined`)
-            }
+            if (!events) err(`transition ${machine.state}:${event} not defined`)
 
             const fn = stateFns[machine.state][event]
-            if (!fn) {
-                err(`transition ${machine.state}:${event} not defined`)
-            }
-
+            if (!fn) err(`transition ${machine.state}:${event} not defined`)
             fn(machine, data)
         }
 
         function setState(state) {
             if (logging) console.log(`transition: ${machine.state} -> ${state}`)
-
             machine.state = state
         }
 
         setState(startState)
-
         return machine
     }
 
     function transition(state, event, fn) {
         if (!stateFns[state]) stateFns[state] = {}
-
         if (stateFns[state][event]) {
             throw new Error(`transition ${state}:${event} already defined`)
         }
-
         stateFns[state][event] = fn
     }
 
-    return {
-        start,
-        transition,
-    }
+    return { start, transition }
 }
